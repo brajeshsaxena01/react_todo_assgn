e893857778a74c03cbc694ac13699443ef7813cf
Brajesh Kumar Saxena <95098581+brajeshsaxena01@users.noreply.github.com>
2022-04-17 17:56:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Todolist from "./components/Todolist";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Todolist />
+    </div>
+  );
+}
